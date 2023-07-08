ad5f46f2b73c3ad026a0c756892c043652c150ba
hyebin <113406555+HyebinHwang@users.noreply.github.com>
2023-07-08 15:29:53 +0900

    Fix: vite config path 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
@@ -5,5 +6,10 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   base: "/pwa/",
 });
