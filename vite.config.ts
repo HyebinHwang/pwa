28ae09116c956c6d39cbbbeafcdf37c2527890a4
hyebin <113406555+HyebinHwang@users.noreply.github.com>
2023-07-08 15:57:00 +0900

    Fix: Vite config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
+console.log("update vite config");
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pwa/",
   plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "/pwa/",
 });
