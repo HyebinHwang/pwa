bf21f865b7e3d4fed407020799148de8b37f612b
hyebin <113406555+HyebinHwang@users.noreply.github.com>
2023-07-08 16:36:58 +0900

    Fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
-console.log("update vite configasd");
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "pwa",
   plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
