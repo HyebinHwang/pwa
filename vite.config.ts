ed5f05932ed9fb93bda860133a2508282a566f6b
hyebin <113406555+HyebinHwang@users.noreply.github.com>
2023-07-08 14:41:04 +0900

    Fix: vite config base 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pwa",
+  base: "/",
   plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
 });
