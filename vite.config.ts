42e986de646244be71413befa8ba0f5914aacfef
hyebin <113406555+HyebinHwang@users.noreply.github.com>
2023-07-10 23:06:02 +0900

    Fix: vite config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,65 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pwa/",
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["**/*"],
+      manifest: {
+        name: "가위바위보게임",
+        short_name: "가위바위보게임",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#fff",
+        description: "RPS game",
+        theme_color: "#fff",
+        icons: [
+          {
+            src: "maskable_icon.png",
+            sizes: "48x48",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "maskable_icon.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "maskable_icon.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "maskable_icon.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "maskable_icon.png",
+            sizes: "168x168",
+            type: "image/png",
+          },
+          {
+            src: "maskable_icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "maskable_icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        related_applications: [
+          {
+            platform: "play",
+            url: "https://play.google.com/store/apps/details?id=cheeaun.hackerweb",
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
