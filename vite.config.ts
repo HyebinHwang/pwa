2ae37a0f1fdaffd7c9dd06213e57128201be5388
hyebin <113406555+HyebinHwang@users.noreply.github.com>
2023-07-11 00:04:05 +0900

    Fix: vite config base 위치 수정

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,13 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/pwa/",
   resolve: {
     alias: {
       find: "@",
       replacement: path.resolve(__dirname, "pwa"),
     },
   },
-  base: "/pwa/",
   plugins: [
     react(),
     tsconfigPaths(),
