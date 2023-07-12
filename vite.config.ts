486c2c1387ee47aa37cb6d7984c4b0922aa78fc2
hyebin <113406555+HyebinHwang@users.noreply.github.com>
2023-07-12 11:09:05 +0900

    Fix: vite PWA 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
@@ -12,64 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "/src/pwa"),
     },
   },
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    VitePWA({
-      includeAssets: ["**/*"],
-      manifest: {
-        name: "가위바위보게임",
-        short_name: "가위바위보게임",
-        start_url: ".",
-        display: "standalone",
-        background_color: "#fff",
-        description: "RPS game",
-        theme_color: "#fff",
-        icons: [
-          {
-            src: "maskable_icon.png",
-            sizes: "48x48",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "maskable_icon.png",
-            sizes: "72x72",
-            type: "image/png",
-          },
-          {
-            src: "maskable_icon.png",
-            sizes: "96x96",
-            type: "image/png",
-          },
-          {
-            src: "maskable_icon.png",
-            sizes: "144x144",
-            type: "image/png",
-          },
-          {
-            src: "maskable_icon.png",
-            sizes: "168x168",
-            type: "image/png",
-          },
-          {
-            src: "maskable_icon.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "maskable_icon.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-        related_applications: [
-          {
-            platform: "play",
-            url: "https://play.google.com/store/apps/details?id=cheeaun.hackerweb",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react(), tsconfigPaths()],
 });
