8c672b643156bfaa2252fbd177fcc7c491a7cccd
hyebin <113406555+HyebinHwang@users.noreply.github.com>
2023-07-12 11:25:12 +0900

    Fix: vite-plugin-pwa 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/pwa/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "/src/pwa"),
-    },
-  },
   plugins: [react(), tsconfigPaths()],
 });
