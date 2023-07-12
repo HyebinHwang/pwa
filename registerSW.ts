21344268296426d77eaa453dd7d989a2da437fb6
hyebin <113406555+HyebinHwang@users.noreply.github.com>
2023-07-12 11:16:37 +0900

    Fix: build test

---
+++ registerSW.ts
@@ -0,0 +1,27 @@
+/// <reference lib="webworker" />
+
+self.addEventListener("install", (event) => {
+  console.log("install!");
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+// activate event
+self.addEventListener("activate", (e) => {
+  console.log("actived", e);
+});
+
+// fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
